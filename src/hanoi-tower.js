4d4dc9dd49534234909849358564f4b98deddda3
lSATAl <70383465+lSATAl@users.noreply.github.com>
2020-10-26 19:38:38 +0300

    fix: add soluttion

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  let a = 2 ** disksNumber - 1
+  let b = 3600 / turnsSpeed
+  let c = a * b
+  let z = {turns : a, seconds: parseInt(c.toPrecision())}
+  return z;
 };
